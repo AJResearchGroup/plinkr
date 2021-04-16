37faa8c42fa4d47d520712d505938b2e9d47936e
richelbilderbeek <richel@richelbilderbeek.nl>
2021-04-16 14:29:05 +0200

    Simplified GHA script

--- tests/testthat/test-get_plink_example_filename.R
+++ tests/testthat/test-get_plink_example_filename.R
@@ -1,4 +1,5 @@
 test_that("use", {
+  if (!is_plink_installed()) return()
   expect_true(file.exists(get_plink_example_filename("test.map")))
   expect_true(file.exists(get_plink_example_filename("test.ped")))
   expect_error(file.exists(get_plink_example_filename("abs.ent")))

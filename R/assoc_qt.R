22c5755bbc145e0c2e9d3f33f9fd0973ca122f1f
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-24 12:43:41 +0200

    Descibe qt_assoc result

--- R/assoc_qt.R
+++ R/assoc_qt.R
@@ -2,6 +2,24 @@
 #'
 #' This function is named after the \code{--assoc} flag used by PLINK
 #' @inheritParams default_params_doc
+#' @return a \link[tibble]{tibble} with the following columns:
+#'
+#'   * \code{trait_name}: name of the quantitive trait,
+#'     taken from the phenotype table column name
+#'   * \code{CHR}: Chromosome number
+#'   * \code{SNP}: SNP identifier
+#'   * \code{BP}: Physical position (base-pair)
+#'   * \code{NMISS}: Number of non-missing genotypes
+#'   * \code{BETA}: Regression coefficient
+#'   * \code{SE}: Standard error
+#'   * \code{R2}: Regression r-squared
+#'   * \code{T}: Wald test (based on t-distribtion)
+#'   * \code{P}: Wald test asymptotic p-value
+#'
+#' The table with have as much rows as the number of SNPs multiplied
+#' by the number of traits.
+#'
+#' Note that parameters in uppercase are named as such by PLINK.
 #' @export
 assoc_qt <- function(
   assoc_qt_params

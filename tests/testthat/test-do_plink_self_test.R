01d3ec8d908852658700cce3bfc8d69011fc062d
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-24 15:08:11 +0200

    Self-test for both versions

--- tests/testthat/test-do_plink_self_test.R
+++ tests/testthat/test-do_plink_self_test.R
@@ -2,3 +2,13 @@ test_that("use", {
   if (!is_plink_installed()) return()
   expect_silent(do_plink_self_test())
 })
+
+test_that("use, v1.7", {
+  if (!is_plink_installed(plink_version = "1.7")) return()
+  expect_silent(do_plink_self_test(plink_version = "1.7"))
+})
+
+test_that("use, v1.9", {
+  if (!is_plink_installed(plink_version = "1.9")) return()
+  expect_silent(do_plink_self_test(plink_version = "1.9"))
+})

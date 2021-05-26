853898930db4fa98ced008bb333bfd21ed99166a
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-26 14:11:32 +0200

    Can set MAFs

--- scripts/scribbles.R
+++ scripts/scribbles.R
@@ -1,5 +1,35 @@
 # Random scribbles
 assoc_qt_params <- create_demo_assoc_qt_params(
+  traits = list(
+    get_random_trait(maf = 0.01),
+    get_random_trait(maf = 0.10),
+    get_random_trait(maf = 0.25)
+  ),
+  n_individuals = 1000
+)
+
+assoc_qt_params <- create_demo_assoc_qt_params(
+  traits = list(
+    get_additive_trait(),
+    get_random_trait()
+  ),
+  n_individuals = 1000
+)
+
+
+assoc_qt_params <- create_demo_assoc_qt_params(
+  traits = get_additive_trait(),
+  n_individuals = 1000
+)
+
+assoc_qt_params <- create_demo_assoc_qt_params(
+  traits = get_random_trait(),
+  n_individuals = 1000
+)
+
+
+
+assoc_qt_params <- create_demo_assoc_qt_params(
   mafs = c(0.01, 0.1, 0.25),
   phenotypes = rep("random", 3),
   n_individuals = 1000
--- tests/testthat/test-create_demo_assoc_qt_params.R
+++ tests/testthat/test-create_demo_assoc_qt_params.R
@@ -3,7 +3,7 @@ test_that("use", {
   expect_silent(check_assoc_qt_params(assoc_qt_params))
 })
 
-test_that("use", {
+test_that("n_individuals", {
   n_individuals <- 31
   assoc_qt_params <- create_demo_assoc_qt_params(
     n_individuals = n_individuals
@@ -11,4 +11,36 @@ test_that("use", {
   expect_silent(check_assoc_qt_params(assoc_qt_params))
   expect_equal(n_individuals, nrow(assoc_qt_params$ped_table))
   expect_equal(n_individuals, nrow(assoc_qt_params$phenotype_table))
+})
+
+test_that("MAFs", {
+  n_individuals <- 100
+  mafs <- c(0.31, 0.02)
+  assoc_qt_params <- create_demo_assoc_qt_params(
+    mafs = mafs,
+    n_individuals = n_individuals,
+  )
+  expect_silent(check_assoc_qt_params(assoc_qt_params))
+  expect_equal(n_individuals, nrow(assoc_qt_params$ped_table))
+  expect_equal(n_individuals, nrow(assoc_qt_params$phenotype_table))
+  # First SNP
+  n_expect_major_alleles <- n_individuals * (1.0 - mafs[1])
+  expect_equal(
+    n_expect_major_alleles,
+    sum(assoc_qt_params$ped_table$snv_1a == "A")
+  )
+  expect_equal(
+    n_expect_major_alleles,
+    sum(assoc_qt_params$ped_table$snv_1b == "A")
+  )
+  # Second SNP
+  n_expect_major_alleles <- n_individuals * (1.0 - mafs[2])
+  expect_equal(
+    n_expect_major_alleles,
+    sum(assoc_qt_params$ped_table$snv_2a == "A")
+  )
+  expect_equal(
+    n_expect_major_alleles,
+    sum(assoc_qt_params$ped_table$snv_2b == "A")
+  )
 })

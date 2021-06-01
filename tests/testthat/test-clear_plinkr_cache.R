50e9a3b42267ec7d80bb07628138017867f0d311
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-01 07:35:03 +0200

    Fix test

--- tests/testthat/test-clear_plinkr_cache.R
+++ tests/testthat/test-clear_plinkr_cache.R
@@ -3,19 +3,22 @@ test_that("minimal use", {
 })
 
 test_that("create file", {
+  plinkr_cache_folder_name <- dirname(get_plinkr_tempfilename())
+
   filename <- get_plinkr_tempfilename()
   dir.create(dirname(filename), showWarnings = FALSE, recursive = TRUE)
   readr::write_lines("irrelevant", file = filename)
-  expect_equal(1, length(list.files(folder)))
+  expect_equal(1, length(list.files(plinkr_cache_folder_name)))
   expect_silent(clear_plinkr_cache())
-  expect_equal(0, length(list.files(folder)))
+  expect_equal(0, length(list.files(plinkr_cache_folder_name)))
 })
 
 test_that("create dir", {
+  plinkr_cache_folder_name <- dirname(get_plinkr_tempfilename())
 
   folder_name <- get_plinkr_tempfilename()
   dir.create(folder_name, showWarnings = FALSE, recursive = TRUE)
-  expect_equal(2, length(list.dirs(folder)))
+  expect_equal(2, length(list.dirs(plinkr_cache_folder_name)))
   expect_silent(clear_plinkr_cache())
-  expect_equal(0, length(list.dirs(folder)))
+  expect_equal(0, length(list.dirs(plinkr_cache_folder_name)))
 })

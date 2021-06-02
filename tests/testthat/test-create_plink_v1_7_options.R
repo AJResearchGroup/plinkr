85e2b69d6b23f512fc6a7fd95acf8f5f644dc981
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-02 15:34:11 +0200

    Fix regex for Win

--- tests/testthat/test-create_plink_v1_7_options.R
+++ tests/testthat/test-create_plink_v1_7_options.R
@@ -2,7 +2,7 @@ test_that("v1.7", {
   plink_options <- create_plink_v1_7_options()
   expect_equal("1.7", plink_options$plink_version)
   expect_true(stringr::str_detect(plink_options$plink_exe_path, "plink_1_7_"))
-  expect_true(stringr::str_detect(plink_options$plink_exe_path, "plink$"))
+  expect_true(stringr::str_detect(plink_options$plink_exe_path, "plink(\\.exe)?$"))
 })
 
 test_that("v1.7", {

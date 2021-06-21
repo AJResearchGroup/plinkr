e1a91f703331093630ae99474f6ad4cda78a2172
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-21 10:33:38 +0200

    EOD

--- R/missing.R
+++ R/missing.R
@@ -1,17 +1,17 @@
-#' Let \code{PLINK}/\code{PLINK2}
-#' calculate the missing rates summary statistic.
-#'
-#' Let \code{PLINK}/\code{PLINK2}
-#' calculate the missing rates summary statistic.
-#' @note This function is named after the \code{--missing} flag
-#' used by \code{PLINK}
-#' @inheritParams default_params_doc
-#' @return a \link[tibble]{tibble}
-#' @author Richèl J.C. Bilderbeek
-#' @export
-missing <- function(
-  bfile,
-  out
-) {
-  stop("Use 'mssing_bfile'")
-}
+#' #' Let \code{PLINK}/\code{PLINK2}
+#' #' calculate the missing rates summary statistic.
+#' #'
+#' #' Let \code{PLINK}/\code{PLINK2}
+#' #' calculate the missing rates summary statistic.
+#' #' @note This function is named after the \code{--missing} flag
+#' #' used by \code{PLINK}
+#' #' @inheritParams default_params_doc
+#' #' @return a \link[tibble]{tibble}
+#' #' @author Richèl J.C. Bilderbeek
+#' #' @export
+#' missing <- function(
+#'   bfile,
+#'   out
+#' ) {
+#'   stop("Use 'mssing_bfile'")
+#' }

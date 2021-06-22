e0f135f68d10f03d463a94007fc1d3bc8864c144
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-22 13:43:30 +0200

    GHA installs PLINK tutorial data

--- scripts/install_plinks.sh
+++ scripts/install_plinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Installs multiple version of PLINK 
+# Installs multiple version of PLINK
 # using the R code in the 'plinkr' package
 #
 # Because the R code in the package uses 'plinkr::' in calling
@@ -13,6 +13,7 @@
 #
 cat R/*.R > tempscript.R
 echo "install_plinks()" >> tempscript.R
+echo "install_plink_tutorial_data()" >> tempscript.R
 sed -i'.orginal' "s/plinkr:://g" tempscript.R
 cat tempscript.R
 Rscript tempscript.R

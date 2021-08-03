4f01fd81fb0dcb9280b69022e52053e9e19fde5e
richelbilderbeek <richel@richelbilderbeek.nl>
2021-08-03 10:12:13 +0200

    Codecov

--- tests/testthat/test-read_plink_assoc_adjusted_file.R
+++ tests/testthat/test-read_plink_assoc_adjusted_file.R
@@ -4,4 +4,9 @@ test_that("use", {
       assoc_adjusted_filename = get_plinkr_filename("assoc.assoc.adjusted")
     )
   )
+  expect_silent(
+    read_plink_assoc_adjusted_file(
+      assoc_adjusted_filename = get_plinkr_filename("assoc.assoc_1_row.adjusted")
+    )
+  )
 })

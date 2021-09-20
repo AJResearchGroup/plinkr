4ab6ffe1cb615bbf3580f6613ed682ed8c72d295
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-20 08:24:34 +0200

    GHA installs Pandoc, add files for https://github.com/richelbilderbeek/gcaer/issues/2

---
+++ scripts/create_gcaer_issue_2_files.R
@@ -0,0 +1,47 @@
+# Needed for https://github.com/richelbilderbeek/gcaer/issues/2
+#
+# Output must be in PLINK binary format
+#
+library(plinkr)
+
+# Simulate data in PLINK1 text format
+assoc_qt_params <- create_demo_assoc_qt_params(
+  n_individuals = 10,
+  traits = create_demo_traits()
+)
+
+# Convert PLINK1 text format to PLINK1 binary format
+base_input_filename <- "~/gcaer_issue_2_text"
+ped_filename <- paste0(base_input_filename, ".ped")
+map_filename <- paste0(base_input_filename, ".map")
+phe_filename <- paste0(base_input_filename, ".phe")
+base_output_filename <- "~/gcaer_issue_2_bin"
+plinkr::save_ped_table_to_file(
+  ped_table = assoc_qt_params$ped_table,
+  ped_filename = ped_filename
+)
+plinkr::save_map_table_to_file(
+  map_table = assoc_qt_params$map_table,
+  map_filename = map_filename
+)
+plinkr::save_phe_table_to_file(
+  phe_table = assoc_qt_params$phe_table,
+  phe_filename = phe_filename
+)
+plink_bin_filenames <- plinkr::convert_plink_text_files_to_plink_bin_files(
+  base_input_filename = base_input_filename,
+  base_output_filename = base_output_filename
+)
+
+# Done!
+
+# But hey, if you want to inspect the data:
+bim_table <- read_plink_bim_file(plink_bin_filenames$bim_filename)
+fam_table <- read_plink_fam_file(plink_bin_filenames$fam_filename)
+bed_table <- read_plink_bed_file(
+  bed_filename = plink_bin_filenames$bed_filename,
+  names_loci = bim_table$id,
+  names_ind = fam_table$id
+)
+
+plinkr::read_plink_bed_file(plink_bin_filenames$bed_filename)

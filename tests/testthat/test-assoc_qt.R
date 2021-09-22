5b8a87aac8d9eb463da05d7eb3f45ea6b716d016
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-22 12:35:26 +0200

    'assoc_qt_params$phe_table' is actually used

--- tests/testthat/test-assoc_qt.R
+++ tests/testthat/test-assoc_qt.R
@@ -48,18 +48,11 @@ test_that("minimal use, using PLINK1 binary data", {
 test_that("minimal use, using PLINK1 binary data", {
   if (!is_plink_installed()) return()
   set.seed(314)
-  assoc_qt_params_as_text_data <- create_test_assoc_qt_params()
-  expect_true(is_plink_text_data(assoc_qt_params_as_text_data$data))
-
-  assoc_qt_params_as_bin_data <- assoc_qt_params_as_text_data
-  assoc_qt_params_as_bin_data$data <- convert_plink_text_data_to_plink_bin_data(
-    assoc_qt_params_as_text_data$data
+  assoc_qt_params <- create_test_assoc_qt_params()
+  assoc_qt_params$data <- convert_plink_text_data_to_plink_bin_data(
+    assoc_qt_params$data
   )
-  expect_true(is_plink_bin_data(assoc_qt_params_as_bin_data$data))
-
-  assoc_qt(
-    assoc_qt_params = assoc_qt_params_as_bin_data
-  )
+  assoc_qt(assoc_qt_params = assoc_qt_params)
   suppressMessages(
     expect_message(
       assoc_qt(
@@ -69,6 +62,34 @@ test_that("minimal use, using PLINK1 binary data", {
       "PLINK"
     )
   )
+
+  expect_silent(check_empty_plinkr_folder())
+})
+
+test_that("minimal use, using PLINK1 binary data and a phenotype file", {
+  if (!is_plink_installed()) return()
+
+  # Goal is to see if the 'assoc_qt_params$phe_table' is actually used,
+  # by doing an association, changing it, and doing an association again
+
+  set.seed(314)
+  assoc_qt_params <- create_test_assoc_qt_params()
+  assoc_qt_params$data <- convert_plink_text_data_to_plink_bin_data(
+    assoc_qt_params$data
+  )
+  before <- assoc_qt(assoc_qt_params = assoc_qt_params)
+
+  assoc_qt_params$phe_table$P1 <- runif(n = nrow(assoc_qt_params$phe_table))
+
+  after <- assoc_qt(assoc_qt_params = assoc_qt_params)
+  expect_equal(before$CHR, after$CHR)
+  expect_equal(before$BP, after$BP)
+  expect_equal(before$NMISS, after$NMISS)
+  expect_true(all(before$BETA != after$BETA))
+  expect_true(all(before$SE != after$SE))
+  expect_true(all(before$R2 != after$R2))
+  expect_true(all(before$T != after$T))
+  expect_true(all(before$P != after$P))
 
   expect_silent(check_empty_plinkr_folder())
 })

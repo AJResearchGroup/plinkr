70a5c2b11e26a2d990758d12f7af4cd690f8a27a
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-24 08:42:25 +0200

    Delete scribbles of bug reports

--- scripts/stringr_bug_report_no_simplify_no_skip_empty_rows_no_str_split_no_str_trim.R
+++
@@ -1,9 +0,0 @@
-remotes::install_github("richelbilderbeek/plinkr")
-
-while(1) {
-  message("Try again")
-  x <- readr::read_lines(
-    plinkr::get_plinkr_filename("frq_within.frq.strat")
-  )
-}
-

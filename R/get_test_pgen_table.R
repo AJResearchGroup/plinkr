dfc62c58111d91fecfcbe5b8279044586ef13f6c
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-30 12:25:44 +0200

    .bed and .pgen tables are transposed, see https://github.com/chrchang/plink-ng/issues/195

--- R/get_test_pgen_table.R
+++ R/get_test_pgen_table.R
@@ -27,32 +27,36 @@ get_test_pgen_table <- function() {
     pgen_table
   }
   # TODO: simplify to work without reading a file
-  if (1 == 2) {
+  if (1 == 1) {
     # An asymmetrical table,
     #   * columns: SNPs
     #   * rows: individuals
-    plinkr::read_plink2_pgen_file_from_files(
-      pgen_filename = plinkr::get_plinkr_filename(
-        "test_v1_7_after_make-bed_after_make-pgen.pgen"
-      ),
-      psam_filename = plinkr::get_plinkr_filename(
-        "test_v1_7_after_make-bed_after_make-pgen.psam"
-      ),
-      pvar_filename = plinkr::get_plinkr_filename(
-        "test_v1_7_after_make-bed_after_make-pgen.pvar"
+    return(
+      plinkr::read_plink2_pgen_file_from_files(
+        pgen_filename = plinkr::get_plinkr_filename(
+          "test_v1_7_after_make-bed_after_make-pgen.pgen"
+        ),
+        psam_filename = plinkr::get_plinkr_filename(
+          "test_v1_7_after_make-bed_after_make-pgen.psam"
+        ),
+        pvar_filename = plinkr::get_plinkr_filename(
+          "test_v1_7_after_make-bed_after_make-pgen.pvar"
+        )
+      )
+    )
+  } else {
+    return(
+      plinkr::read_plink2_pgen_file_from_files(
+        pgen_filename = plinkr::get_plinkr_filename(
+          "toy_v1_9_after_make-bed_after_make-pgen.pgen"
+        ),
+        psam_filename = plinkr::get_plinkr_filename(
+          "toy_v1_9_after_make-bed_after_make-pgen.psam"
+        ),
+        pvar_filename = plinkr::get_plinkr_filename(
+          "toy_v1_9_after_make-bed_after_make-pgen.pvar"
+        )
       )
     )
   }
-
-  plinkr::read_plink2_pgen_file_from_files(
-    pgen_filename = plinkr::get_plinkr_filename(
-      "toy_v1_9_after_make-bed_after_make-pgen.pgen"
-    ),
-    psam_filename = plinkr::get_plinkr_filename(
-      "toy_v1_9_after_make-bed_after_make-pgen.psam"
-    ),
-    pvar_filename = plinkr::get_plinkr_filename(
-      "toy_v1_9_after_make-bed_after_make-pgen.pvar"
-    )
-  )
 }
--- tests/testthat/test-convert_pgen_table_to_bed_table.R
+++ tests/testthat/test-convert_pgen_table_to_bed_table.R
@@ -3,7 +3,6 @@ test_that("use", {
   bed_table <- convert_pgen_table_to_bed_table(
     pgen_table = pgen_table
   )
-  expect_equal(nrow(pgen_table), nrow(bed_table))
-  expect_equal(ncol(pgen_table), ncol(bed_table))
-  expect_identical(pgen_table, bed_table)
+  # .bed and .pgen tables are transposed, see https://github.com/chrchang/plink-ng/issues/195
+  expect_identical(pgen_table, t(bed_table))
 })

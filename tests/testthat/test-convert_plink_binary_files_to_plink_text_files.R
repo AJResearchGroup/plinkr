429ab49df0240c46993f414b3691bc94f6b90faa
richelbilderbeek <richel@richelbilderbeek.nl>
2021-10-01 16:01:45 +0200

    EOD

--- tests/testthat/test-convert_plink_binary_files_to_plink_text_files.R
+++ tests/testthat/test-convert_plink_binary_files_to_plink_text_files.R
@@ -3,6 +3,9 @@ test_that("use", {
   expect_silent(check_empty_plinkr_folder())
 
   skip("TODO: convert_plink_bin_files_to_plink_text_files")
+  base_input_filename <- tools::file_path_sans_ext(
+    get_plinkr_filename("test_v1_7_after_make-bed.bed")
+  )
   convert_plink_bin_files_to_plink_text_files(
     base_input_filename,
     base_output_filename = file.path(dirname(base_input_filename), "output"),

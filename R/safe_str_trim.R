a1a4001e0ff4b58962e9198cde77088cef36927b
richelbilderbeek <richel@richelbilderbeek.nl>
2021-10-04 15:07:08 +0200

    stringi v1.7.5 is on CRAN

--- R/safe_str_trim.R
+++ R/safe_str_trim.R
@@ -1,17 +1,9 @@
 #' Call \link[stringr]{str_trim} safely
-#' stringi::str_trim **sometimes** gives an 'embedded nul in string' error.
+#' stringi::str_trim **sometimes** gave an 'embedded nul in string' error.
 #' This has been reported at https://github.com/gagolews/stringi/issues/458 .
 #' Until then, just try multiple times :-)
 #' @param string one or more strings
 #' @export
 safe_str_trim <- function(string) {
-  return(stringr::str_trim(string))
-
-  stop("Old code")
-  while (1) {
-    tryCatch(
-      return(stringr::str_trim(as.character(string))),
-      error = function(e) {} # nolint ignore
-    )
-  }
+  stringr::str_trim(string)
 }

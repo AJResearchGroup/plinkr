fc97e9a1c7bd2e774b37e54b79038bf41dbb8a09
richelbilderbeek <richel@richelbilderbeek.nl>
2021-10-08 10:42:11 +0200

    Do speed comparison, takes 40 mins

--- scripts/speed_comparison.R
+++ scripts/speed_comparison.R
@@ -1,9 +1,9 @@
 tibbles <- list()
 
 data <- tidyr::expand_grid(
-  n_individuals = c(10, 100),
+  n_individuals = c(10, 100, 1000),
   n_phenotypes = c(1, 10, 100),
-  n_snps_per_phenotype = c(1, 10, 100)
+  n_snps_per_phenotype = c(1, 10, 100, 1000)
 )
 for (i in seq_len(nrow(data))) {
   message(Sys.time(), ": ", i, "/" ,nrow(data))
@@ -12,8 +12,9 @@ for (i in seq_len(nrow(data))) {
     n_phenotypes = data$n_phenotypes[i],
     n_snps_per_phenotype = data$n_snps_per_phenotype[i]
   )
-  fastest_row_index <- which.min(t$time_sec)
-  t_best <- t[fastest_row_index, ]
+  # fastest_row_index <- which.min(t$time_sec)
+  # t_best <- t[fastest_row_index, ]
+  t_best <- t
   t_best$n_invididuals <- data$n_individuals[i]
   t_best$n_phenotypes <- data$n_phenotypes[i]
   t_best$n_snps_per_phenotype <- data$n_snps_per_phenotype[i]

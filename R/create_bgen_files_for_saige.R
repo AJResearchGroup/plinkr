d42b188e8ca56a0be3c6ee09353cf4a3a75cab65
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-24 13:18:17 +0100

    Start of #32, thanks @mkierczak

---
+++ R/create_bgen_files_for_saige.R
@@ -0,0 +1,7 @@
+#' Create the `.bgen` files for `SAIGE`
+#' @note thanks to Marcin Kierczak  (\url{https://github.com/mkierczak/})
+#' for sharing his wisdom
+#' @export
+create_bgen_files_for_saige <- function() {
+  # Use --export bgen-1.2 bits=8
+}
---
+++ tests/testthat/test-create_bgen_files_for_saige.R
@@ -0,0 +1,3 @@
+test_that("use", {
+  expect_silent(create_bgen_files_for_saige())
+})

9ffe83bc2f0785dc4972e77015cb449ee7c8a40d
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-21 08:42:23 +0100

    Clear cache

--- tests/testthat/test-convert_plink_bin_data_to_plink2_bin_data.R
+++ tests/testthat/test-convert_plink_bin_data_to_plink2_bin_data.R
@@ -1,7 +1,8 @@
 test_that("minimal use", {
-  expect_silent(check_empty_plinkr_folder())
-
+  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
   if (!is_plink_installed(plink_options = create_plink_v2_0_options())) return()
+
+  clear_plinkr_cache()
 
   expect_true(
     is_plink2_bin_data(
@@ -12,14 +13,14 @@ test_that("minimal use", {
   )
 
   expect_silent(check_empty_plinkr_folder())
-  clear_plinkr_cache() # nolint
 })
 
 test_that("PLINK1 does not convert PLINK1 bin to PLINK2 binary", {
-  expect_silent(check_empty_plinkr_folder())
-
+  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
   if (!is_plink_installed(plink_options = create_plink_v1_7_options())) return()
   if (!is_plink_installed(plink_options = create_plink_v1_9_options())) return()
+
+  clear_plinkr_cache()
 
   expect_error(
     convert_plink_bin_data_to_plink2_bin_data(
@@ -37,5 +38,4 @@ test_that("PLINK1 does not convert PLINK1 bin to PLINK2 binary", {
   )
 
   expect_silent(check_empty_plinkr_folder())
-  clear_plinkr_cache() # nolint
 })

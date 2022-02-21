ac3b708fbc39c2cdcc809d764190f124b4d5a535
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-21 08:15:10 +0100

    Increase codecov

--- tests/testthat/test-read_plink2_qassoc_files.R
+++ tests/testthat/test-read_plink2_qassoc_files.R
@@ -8,3 +8,14 @@ test_that("use", {
   expect_true("additive" %in% t$PHENO)
   expect_true("random" %in% t$PHENO)
 })
+
+test_that("reading an absent file results in an empty table", {
+  t_exists <- read_plink2_qassoc_files(
+    qassoc_filenames = get_plinkr_filename(
+      "plink2_assoc_output.additive.glm.linear")
+  )
+  t_absent <- read_plink2_qassoc_files(
+    qassoc_filenames = "absent"
+  )
+  expect_equal(names(t_exists), names(t_absent))
+})

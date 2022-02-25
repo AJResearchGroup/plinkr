349548de83a80ab1fec54b85f34ebcd0fc544226
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-25 08:07:38 +0100

    Add doc

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# Run the Singularity container on a demo R script
+#
+# Usage
+#
+# ./scripts/run_container.sh
+#
 # --cleanenv: recommened by tsnowlan at https://stackoverflow.com/a/71252619
 # --bind $PWD/scripts/ : bind the folder, so that it works on GitHub Actions as well
 singularity run --cleanenv --bind $PWD/scripts/ plinkr.sif scripts/demo_container.R

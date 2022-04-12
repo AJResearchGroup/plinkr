0232492ea7cb056f745afdf3d53ae20d9b9c4a8e
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-12 16:43:58 +0200

    Fix tests

--- tests/testthat/test-plink_assoc.R
+++ tests/testthat/test-plink_assoc.R
@@ -4,6 +4,7 @@ test_that("minimal use, v1.7", {
   if (!is_plink_installed()) return()
   expect_silent(
     plink_assoc(
+      assoc_data = create_test_assoc_data(),
       assoc_params = create_test_assoc_params(),
       plink_options = create_plink_v1_7_options()
     )
@@ -17,6 +18,7 @@ test_that("minimal use, v1.9", {
   set.seed(314)
   expect_silent(
     plink_assoc(
+      assoc_data = create_test_assoc_data(),
       assoc_params = create_test_assoc_params(),
       plink_options = create_plink_v1_9_options()
     )
@@ -27,7 +29,11 @@ test_that("verbose", {
   if (!is_plink_installed()) return()
   set.seed(314)
   expect_message(
-    plink_assoc(assoc_params = create_test_assoc_params(), verbose = TRUE)
+    plink_assoc(
+      assoc_data = create_test_assoc_data(),
+      assoc_params = create_test_assoc_params(),
+      verbose = TRUE
+    )
   )
   expect_silent(check_empty_plinkr_folder())
 })
@@ -36,7 +42,11 @@ test_that("confidence interval", {
   if (!is_plink_installed()) return()
   set.seed(314)
   expect_message(
-    plink_assoc(create_test_assoc_params(), verbose = TRUE),
+    plink_assoc(
+      assoc_data = assoc_data,
+      assoc_params = create_test_assoc_params(),
+      verbose = TRUE
+    ),
     "--ci"
   )
 })
@@ -45,7 +55,10 @@ test_that("use, test", {
   if (!is_plink_installed()) return()
   set.seed(314)
   assoc_params <- create_test_assoc_params()
-  assoc_result <- plink_assoc(assoc_params = assoc_params)
+  assoc_result <- plink_assoc(
+    assoc_data = assoc_data,
+    assoc_params = create_test_assoc_params(),
+  )
   expect_true("assoc_table" %in% names(assoc_result))
   expect_true("log" %in% names(assoc_result))
   expect_silent(check_empty_plinkr_folder())
@@ -55,7 +68,10 @@ test_that("use, demo", {
   if (!is_plink_installed()) return()
   set.seed(317)
   assoc_data <- create_demo_assoc_data()
-  assoc_results <- assoc(assoc_params = assoc_params)
+  assoc_results <- assoc(
+    assoc_data = assoc_data,
+    assoc_params = create_test_assoc_params(),
+  )
   # 1 traits times 1 SNP = 1 association
   expect_equal(1, nrow(assoc_results$assoc_table))
 })
@@ -65,7 +81,10 @@ test_that("demo on random only", {
   assoc_data <- create_demo_assoc_data(
     trait = create_random_case_control_trait()
   )
-  assoc_results <- plink_assoc(assoc_params = assoc_params)
+  assoc_results <- plink_assoc(
+    assoc_data = assoc_data,
+    assoc_params = create_test_assoc_params(),
+  )
   # 1 trait times 1 SNP = 1 association
   expect_equal(nrow(assoc_results$assoc_table), 1)
 })
@@ -77,7 +96,10 @@ test_that("number of individuals", {
     n_individuals = 5,
     trait = create_random_case_control_trait()
   )
-  assoc_results <- assoc(assoc_params = assoc_params)
+  assoc_results <- assoc(
+    assoc_data = assoc_data,
+    assoc_params = create_test_assoc_params(),
+  )
   # One traits times one SNP = one association
   expect_equal(1, nrow(assoc_results$assoc_table))
   expect_silent(check_empty_plinkr_folder())
@@ -85,12 +107,13 @@ test_that("number of individuals", {
 
 test_that("error when case-controls are not 1 or 2", {
   if (!is_plink_installed()) return()
-  assoc_params <- create_test_assoc_params()
-  n_individuals <- nrow(assoc_params$data$ped_table)
-  assoc_params$data$ped_table$case_control_code <- 314
+  assoc_data <- create_test_assoc_data()
+  n_individuals <- nrow(assoc_data$data$ped_table)
+  assoc_data$data$ped_table$case_control_code <- 314
   expect_error(
     plink_assoc(
-      assoc_params = assoc_params,
+      assoc_data = assoc_data,
+      assoc_params = create_test_assoc_params(),
       verbose = TRUE
     )
   )
@@ -105,7 +128,8 @@ test_that("PLINK cannot handle triallelic SNPs", {
   )
   expect_warning(
     plink_assoc(
-      assoc_params = assoc_params
+      assoc_data = assoc_data,
+      assoc_params = create_test_assoc_params(),
     ),
     "Variant 1 triallelic; setting rarest alleles missing."
   )
@@ -120,7 +144,8 @@ test_that("PLINK cannot handle quadallelic SNPs", {
   )
   expect_warning(
     plink_assoc(
-      assoc_params = assoc_params
+      assoc_data = assoc_data,
+      assoc_params = create_test_assoc_params(),
     ),
     "Variant 1 quadallelic; setting rarest alleles missing"
   )
@@ -136,7 +161,10 @@ test_that("All 95 chromosome numbers work", {
     trait = create_random_case_control_trait(n_snps = 95),
     n_individuals = 10
   )
-  plink_assoc(assoc_params = assoc_params)
+  plink_assoc(
+    assoc_data = assoc_data,
+    assoc_params = create_test_assoc_params(),
+  )
 
   expect_silent(check_empty_plinkr_folder())
   clear_plinkr_cache()

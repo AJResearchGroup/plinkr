61a1b107b0c1fd50fbf6f9f0358104981e48198f
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-13 08:04:16 +0200

    Fix Singularity demo script

--- scripts/demo_container.R
+++ scripts/demo_container.R
@@ -1,7 +1,8 @@
 library(plinkr)
 plink_optionses <- create_plink_optionses(plink_folder = "/opt/plinkr")
 assoc(
-  assoc_params = create_demo_assoc_data(),
+  assoc_data = create_demo_assoc_data(),
+  assoc_params = create_test_assoc_params(),
   plink_options = plink_optionses[[2]]
 )
 

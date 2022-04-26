a14c8ecc9d294f5ef922a37cfd1555b94e2a52cf
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-26 12:06:12 +0200

    Fix test

--- tests/testthat/test-get_test_pgen_table.R
+++ tests/testthat/test-get_test_pgen_table.R
@@ -1,9 +1,16 @@
 test_that("musy match table as loaded from file", {
   pgen_table <- get_test_pgen_table()
-  pgen_table_again <- read_plink2_pgen_file(
-    get_plinkr_filename("test_v1_7_after_make-bed_after_make-pgen.pgen"),
-    names_loci = c("snp1", "snp2"),
-    names_ind = seq(1, 6)
+
+  pgen_table_again <- plinkr::read_plink2_pgen_file_from_files(
+    pgen_filename = plinkr::get_plinkr_filename(
+      "toy_v1_9_after_make-bed_after_make-pgen.pgen"
+    ),
+    psam_filename = plinkr::get_plinkr_filename(
+      "toy_v1_9_after_make-bed_after_make-pgen.psam"
+    ),
+    pvar_filename = plinkr::get_plinkr_filename(
+      "toy_v1_9_after_make-bed_after_make-pgen.pvar"
+    )
   )
   expect_identical(pgen_table, pgen_table_again)
 })

ecf90842e5965028da212df837050a9578d9de75
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-03 09:44:00 +0200

    Fix style

--- tests/testthat/test-create_assoc_qt_args_on_plink_bin_data.R
+++ tests/testthat/test-create_assoc_qt_args_on_plink_bin_data.R
@@ -13,8 +13,8 @@ test_that("--allow-no-sex", {
   assoc_qt_data <- create_test_assoc_qt_data(
     data = create_test_plink_bin_data()
   )
-  assoc_qt_params_false = create_test_assoc_qt_params(allow_no_sex = FALSE)
-  assoc_qt_params_true = create_test_assoc_qt_params(allow_no_sex = TRUE)
+  assoc_qt_params_false <- create_test_assoc_qt_params(allow_no_sex = FALSE)
+  assoc_qt_params_true <- create_test_assoc_qt_params(allow_no_sex = TRUE)
   args_true <- create_assoc_qt_args_on_plink_bin_data(
     assoc_qt_data = assoc_qt_data,
     assoc_qt_params = assoc_qt_params_true
